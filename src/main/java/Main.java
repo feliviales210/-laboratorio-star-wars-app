84ad972375321adcd08edafb64b82447dfdab4ed
vivallo <andresvivallo4@gmail.com>
2021-10-28 16:32:55 -0600

    [ADD] Add main window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JTable table = new JTable();
+
+        frame.add(table);
+
+        frame.setTitle("Star Wars App");
+        frame.setSize(600, 600);
+        frame.setLayout(null);
+        frame.setVisible(true);
         System.out.println("Hello world");
     }
 
