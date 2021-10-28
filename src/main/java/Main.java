416cafad7ff7224cb8b57d7c5b96987026fa82d9
vivallo <andresvivallo4@gmail.com>
2021-10-28 17:07:33 -0600

    [ADD] JSON parsing with Jackson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,41 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JTable table = new JTable();
 
-        frame.add(table);
+        logger.info("Frame created");
 
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            // convert JSON file to map
+            Map<?, ?> map = mapper.readValue(Paths.get("src/main/resources/data.json").toFile(), Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        frame.add(table);
         frame.setTitle("Star Wars App");
         frame.setSize(600, 600);
         frame.setLayout(null);
         frame.setVisible(true);
-        System.out.println("Hello world");
     }
 
 
